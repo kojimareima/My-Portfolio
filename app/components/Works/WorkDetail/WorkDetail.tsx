ba8a19b86994efdba57c0340c9d3327a510fbef6
Toshiki Takakubo <219tosiki@gmail.com>
2023-12-01 10:38:21 +0900

    画像モーダルの作成

--- app/components/Works/WorkDetail/WorkDetail.tsx
+++ app/components/Works/WorkDetail/WorkDetail.tsx
@@ -1,9 +1,52 @@
-import { Card, CardBody, Image } from "@nextui-org/react";
-import React from "react";
+"use client";
+import {
+  Card,
+  Image,
+  Modal,
+  ModalBody,
+  ModalContent,
+  useDisclosure,
+} from "@nextui-org/react";
+import React, { useState } from "react";
 import data from "@/app/components/Works/WorkDetail/WorkDetail.json";
+
+interface ImageModalProps {
+  image: string;
+}
 
 const WorkDetail = ({ params }: { params: { id: string } }) => {
   const filteredData = data.filter((item) => item.id === params.id);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [selectedImage, setSelectedImage] = useState("");
+
+  const ImageModal: React.FC<ImageModalProps> = ({ image }) => (
+    <Modal isOpen={isOpen} onClose={onOpenChange} backdrop="blur" size="4xl">
+      <ModalContent>
+        <ModalBody>
+          <Image
+            shadow="sm"
+            radius="lg"
+            width="98%"
+            alt={work.name}
+            className="w-full object-cover"
+            src={image}
+          />
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+
+  const handleCardClick = (image: string) => {
+    setSelectedImage(image);
+    onOpen();
+  };
+
+  const renderActiveModal = () => {
+    if (selectedImage) {
+      return <ImageModal image={selectedImage} />;
+    }
+    return null;
+  };
 
   if (filteredData.length === 0) {
     return <div>指定されたIDの作品が見つかりません。</div>;
@@ -12,27 +55,29 @@ const WorkDetail = ({ params }: { params: { id: string } }) => {
   const work = filteredData[0];
 
   return (
-    <div>
+    <div className="m-4">
       <div>
         <div className="text-3xl mt-4 font-semibold">{work.name}</div>
         <div>{work.description}</div>
       </div>
-      <div className="gap-6 grid grid-cols-2 sm:grid-cols-2 mt-4">
+      <div className="gap-6 grid grid-cols-2 sm:grid-cols-3 mt-4">
         {work.images.map((image, index) => (
-          <Card shadow="sm" key={index}>
-            <CardBody className="overflow-visible p-0">
-              <Image
-                shadow="sm"
-                radius="lg"
-                width="100%"
-                alt={work.name}
-                className="w-full object-cover"
-                src={image}
-              />
-            </CardBody>
+          <Card
+            shadow="sm"
+            key={index}
+            isPressable
+            onPress={() => handleCardClick(image)}
+          >
+            <Image
+              removeWrapper
+              alt={work.name}
+              src={image}
+              className="w-full h-full"
+            />
           </Card>
         ))}
       </div>
+      {renderActiveModal()}
     </div>
   );
 };
